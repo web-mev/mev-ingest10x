6cedf4b2672e9a2010866f60b56f4647d4adc114
dkdeconti <deconti.d@gmail.com>
2022-05-23 20:07:40 -0400

    Created a CellRanger import to TSV script.

---
+++ docker/ingest_10x.R
@@ -0,0 +1,91 @@
+suppressMessages(suppressWarnings(library("singleCellTK")))
+suppressMessages(suppressWarnings(library("Matrix")))
+suppressMessages(suppressWarnings(library("optparse")))
+
+# args from command line
+args <- commandArgs(TRUE)
+
+option_list <- list(
+    make_option(
+        c("-b", "--barcodes"),
+        help="CellRanger barcodes.tsv.gz"
+    ),
+    make_option(
+        c("-f", "--features"),
+        help="CellRanger features.tsv.gz"
+    ),
+    make_option(
+        c("-m", "--matrix"),
+        help="CellRanger matrix.mtx.gz"
+    ),
+    make_option(
+        c("-s", "--sample_name"),
+        help="Sample name"
+    ),
+    make_option(
+        c("-o", "--output_file"),
+        help="Output TSV file name"
+    )
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+# set the dummy parent folder
+parent_dir = "webmev_cellranger_import"
+
+# Check that the file was provided:
+if (is.null(opt$barcodes)){
+    message(
+        'Need to provide a barcodes.tsv.gz file with the -b/--barcodes arg.'
+    )
+    quit(status=1)
+}
+if (is.null(opt$features)){
+    message(
+        'Need to provide a features.tsv.gz file with the -f/--features arg.'
+    )
+    quit(status=1)
+}
+if (is.null(opt$matrix)){
+    message(
+        'Need to provide a matrix.mtx.gz file with the -m/--matrix arg.'
+    )
+    quit(status=1)
+}
+
+# Dummy out the CellRanger file structure
+cellranger_path = paste(
+    ".",
+    parent_dir,
+    opt$sample_name,
+    "outs/filtered_feature_bc_matrix",
+    sep="/"
+)
+dir.create(cellranger_path)
+
+# Fills downstream folder with data
+file.copy(
+    c(opt$barcodes, opt$features, opt$matrix),
+    cellranger_path
+)
+
+# import CellRanger
+# It does not matter if filtered or not, 
+# as we made a dummy CellRanger directory
+sce <- importCellRanger(
+    cellRangerDirs = parent_dir,
+    sampleDirs = opt$sample_name,
+    sampleNames = NULL,
+    dataType = "filtered"
+)
+
+# Convert sce counts to non-sparse matrix
+counts <- as.matrix(counts(sce))
+
+# Export counts to TSV
+write.table(
+    counts,
+    file = opt$output_file,
+    sep = "\t",
+    quote = F
+)
